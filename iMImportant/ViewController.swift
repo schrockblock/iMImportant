6e41c54c09d3a7a6de30a4de80894f20bcfbdf80
Elliot Schrock <elliot.schrock@meyouhealth.com>
2014-10-27 15:15:14 -0400

    Initial Commit

---
+++ iMImportant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iMImportant
+//
+//  Created by Elliot Schrock on 10/27/14.
+//  Copyright (c) 2014 MeYou Health. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
